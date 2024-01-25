c4f3d00c57f0b89e006d121f7b9f0704e8d34dc9
Samanyu-T <samanyut8@gmail.com>
2024-01-25 11:15:33 +0000

    Update parallel_optimize.sh

--- parallel_optimize.sh
+++ parallel_optimize.sh
@@ -1,8 +1,13 @@
 nproc=28
+rm -rf ../Output
+mkdir ../Output
+
+rm -rf ../Error
+mkdir ../Error
 
 for ((i = 0; i < nproc; i++)); do
     
-    python "Python_Scripts/Optimize.py" $i > "../output.$i.txt" 2> "../error.$i.txt" &
+    python "Python_Scripts/Optimize.py" $i > "../Output/output.$i.txt" 2> "../Error/error.$i.txt" &
 
 done
 
