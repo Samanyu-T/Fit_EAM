9208cf84a3af5226f5538d36d1ff5f842a68d31a
Samanyu-T <samanyut8@gmail.com>
2024-02-06 16:19:27 +0000

    Update python_gmm_optim.py

--- Python_Scripts/python_gmm_optim.py
+++ Python_Scripts/python_gmm_optim.py
@@ -5,6 +5,7 @@ import Simplex
 import GMM
 import sys
 import json
+import time
 
 def main(machine, max_time):
 
@@ -22,6 +23,8 @@ def main(machine, max_time):
         print('Start Random Sampling')
         sys.stdout.flush()  
 
+    t1 = time.perf_counter()
+
     try:
         Random_Sampling.optimize(n_knots=n_knots, bool_fit=bool_fit, proc=me, machine=machine, max_time=max_time)
     except Exception as e:
@@ -30,10 +33,13 @@ def main(machine, max_time):
                 error_file.write(str(e))
 
     comm.barrier()
+    t2 = time.perf_counter()
 
     if me == 0:
-        print('Start GMM Clustering')
+        print('Random Sampling took %.2f s \n Start GMM Clustering' % (t2 - t1))
         sys.stdout.flush()  
+
+    t1 = time.perf_counter()
 
     try:
         if me == 0:
@@ -45,10 +51,13 @@ def main(machine, max_time):
 
     comm.barrier()
 
+    t2 = time.perf_counter()
 
     if me == 0:
-        print('Start Gaussian Sampling')
+        print('Clustering took %.2f s \n Start Gaussian Sampling' % (t2 - t1))
         sys.stdout.flush()  
+
+    t1 = time.perf_counter()
 
     try:
         Gaussian_Sampling.optimize(n_knots=n_knots, bool_fit=bool_fit, proc=me, machine=machine, max_time=max_time)
@@ -56,11 +65,12 @@ def main(machine, max_time):
         if me == 0:
             with open('../Error/gaussian.txt', 'w') as error_file:
                 error_file.write(str(e))
+    t2 = time.perf_counter()
 
     comm.barrier()
 
     if me == 0:
-        print('Start Simplex')
+        print('Gaussian Sampling took %.2f s \n Start Simplex' % (t2 - t1))
         sys.stdout.flush()  
 
     try:
