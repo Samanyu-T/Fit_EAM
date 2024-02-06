50ca78ca0b2a79dadb2deaab1017c9a08db924aa
Samanyu-T <samanyut8@gmail.com>
2024-02-06 14:47:01 +0000

    update

--- Python_Scripts/Random_Sampling.py
+++ Python_Scripts/Random_Sampling.py
@@ -89,7 +89,7 @@ def optimize(n_knots, bool_fit, proc, machine, max_time=11):
 
     N_samples = int(T_max//t_iter)
 
-    print('The number of divisions made on each dim: %.2f' % N_samples**(1/n_params))
+    print('The number of divisions made on each dim: %.2f' % N_samples)
 
     # Read Daniel's potential to initialize the W-H potential and the params for writing a .eam.alloy file
     pot, starting_lines, pot_params = read_pot('Potentials/WHHe_test.eam.alloy')
